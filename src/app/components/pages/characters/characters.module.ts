e062e8a326f23cb14163e37544001fb4e779e4f3
Sergio Ricardo Suárez Bohórquez <ssuarez@aguasdemanizales.com.co>
2021-12-16 22:38:14 -0500

    Se agrega el proyecto al repositorio

---
+++ src/app/components/pages/characters/characters.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { CharacterListComponent } from '@characters/character-list/character-list.component';
+import { CharacterDetailsComponent } from '@characters/character-details/character-details.component';
+import { CharacterComponent } from '@characters/character.component';
+
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+const myComponents = [CharacterDetailsComponent, CharacterListComponent, CharacterComponent];
+
+@NgModule({
+  declarations: [ ...myComponents],
+  imports: [CommonModule, RouterModule, InfiniteScrollModule],
+  exports: [...myComponents]
+})
+export class CharactersModule { }
